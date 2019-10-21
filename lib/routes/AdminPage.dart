57a4a2adcaebc3ea8fa5e8664a5b679e54505d08
Guillermo Canales <memoadian@gmail.com>
2019-10-21 01:49:31 -0500

    tabs

--- lib/routes/AdminPage.dart
+++ lib/routes/AdminPage.dart
@@ -1,20 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:lab_02/routes/AddPetPage.dart';//importamos Añadir amigo
 
 class AdminPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Administrar Amigos"),
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            alignment: Alignment.center,
-            child: Text('Admin Page'),
+    /*
+     * llamamos una función que retornará el widget
+     * que contiene las tabs de navegación entre un
+     * contenido y otro
+    */
+    return tabs(context);
+  }
+
+  Widget tabs (BuildContext context) {
+    return DefaultTabController(//este Widget es el que crea las tabs
+        length: 2,//pasamos el numero de tabs a mostrar
+        child: Scaffold(//retornamos un scaffold
+          appBar: AppBar(//colocamos el appbar aquí adentro
+            bottom: TabBar(//colocamos el tabbar
+              tabs: [//array de tabs
+                Tab(text: 'Favoritos'),//texto de la pestaña
+                Tab(text: 'Todos'),//texto de la pestaña
+              ],
+            ),
+            title: Text('Más Petamigos'),//titulo de la appbar
           ),
-        ],
+          body: TabBarView(//body del tabbar controller
+            children: [//array (debe ser el mismo que se declara en length)
+              favs(),//función favoritos
+              server(),//función server
+            ],
+          ),
+          //declaramos un floating button
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {//evento press
+              Navigator.push(context,
+                MaterialPageRoute(
+                  builder: (context) => AddPetPage(),//navegamos a añadir amigo
+                ),
+              );
+            },
+            child: Icon(Icons.add),//ícono del botón
+            backgroundColor: Colors.blue,//color del botón
+          ),
       ),
     );
+  }
+
+  Widget favs () {
+    //retornamos solo un texto
+    return Text('aquí van mis favoritos');
+  }
+
+  Widget server () {
+    //retornamos solo un texto
+    return Text('aquí van los datos del web server');
   }
 }
--- lib/routes/DetailPetPage.dart
+++ lib/routes/DetailPetPage.dart
@@ -4,14 +4,38 @@ class DetailPetPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            alignment: Alignment.center,
-            child: Text('Detalle Amigo'),
-          ),
-        ],
+      appBar: AppBar(
+        title: Text("Detalle de Amigo"),
       ),
+      body: Container(
+        child: Card(//creamos una card
+          margin: EdgeInsets.all(10.0),//margen de 10
+          child: Column(//creamos una columna para colocar varios hijos
+            mainAxisSize: MainAxisSize.min,//definimos ualtura ajustable a contenido
+            children: <Widget>[//array
+              Container (//contenedor de imagen
+                padding: EdgeInsets.all(10.0),//padding
+                child: Image.asset('images/logo_flutter.png'),//imagen interna
+              ),
+              Container (//contenedor de texto
+                padding: EdgeInsets.all(10.0),//padding
+                child: Text('Flutter 2',//título
+                  style: TextStyle(fontSize: 18)//estilo del texto
+                ),
+              ),
+              Container(
+                padding: EdgeInsets.all(10.0),
+                child: Text(//descripción
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'+
+                  'Ut blandit porta lectus, ut vulputate ligula maximus quis.'+
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'+
+                  'Ut blandit porta lectus, ut vulputate ligula maximus quis.'
+                ),
+              )
+            ],
+          ),
+        ),
+      )
     );
   }
 }
